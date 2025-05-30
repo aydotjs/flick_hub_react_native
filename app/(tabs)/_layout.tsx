f843a9c1494fab997ad5b81583fdf659622ffd1a
aydotjs <ayobami66@gmail.com>
2025-05-30 23:46:15 +0100

    styled home navigation tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, Image} from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const _layout = () => {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ headerShown: false, title: "Home" }}
+        options={{ headerShown: false, title: "Home",  tabBarIcon: ({focused}) =>  (
+            <>
+            <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 justify-center items-center rounded-full overflow-hidden">
+                 <Image source={icons.home} tintColor="#151312" className="size-5"/>
+                 <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
+            </ImageBackground>
+           
+            </>
+        ) }}
+       
       />
       <Tabs.Screen
         name="profile"
